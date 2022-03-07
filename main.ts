7a8ad5a25a6cb9a84cad22a9071669073e07bbe6
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2022-03-07 12:49:08 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    servos.P0.setAngle(150)
+    servos.P0.setAngle(70)
+})
+input.onSound(DetectedSound.Loud, function () {
+    soundExpression.giggle.playUntilDone()
+    for (let index = 0; index < 4; index++) {
+        servos.P0.setAngle(70)
+        basic.pause(200)
+        servos.P0.setAngle(30)
+        basic.pause(200)
+    }
+    servos.P0.setAngle(50)
 })
 input.onButtonPressed(Button.AB, function () {
-    servos.P0.setAngle(90)
+    servos.P0.setAngle(30)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P0.setAngle(50)
 })
-input.onPinPressed(TouchPin.P1, function () {
-    for (let index = 0; index < 4; index++) {
-        servos.P0.setAngle(50)
-        basic.pause(200)
-        servos.P0.setAngle(150)
-        basic.pause(200)
-    }
-    servos.P0.setAngle(90)
-})
-servos.P0.setAngle(90)
+servos.P0.setAngle(50)
+input.setSoundThreshold(SoundThreshold.Loud, 179)
